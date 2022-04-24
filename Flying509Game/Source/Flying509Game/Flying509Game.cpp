6adeffed95d42a424becd92879576fb63dc74f23
liebeziv <42361683+liebeziv@users.noreply.github.com>
2022-04-25 00:27:14 +0100

    Del

--- Flying509Game/Source/Flying509Game/Flying509Game.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Flying509Game.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Flying509Game, "Flying509Game" );
- 
